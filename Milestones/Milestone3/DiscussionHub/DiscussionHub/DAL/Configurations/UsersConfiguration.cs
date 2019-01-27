58d967b43f88e4e6ab3e996802d2aaa650e883f0
Shayna Conner <sconner15@wou.edu>
2019-01-27 15:05:52 -0800

    EF models and config

---
+++ Milestones/Milestone3/DiscussionHub/DiscussionHub/DAL/Configurations/UsersConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using DiscussionHub.Models;
+
+namespace DiscussionHub.DAL.Configurations
+{
+    public class UsersConfiguration : EntityTypeConfiguration<Users>
+    {
+        public UsersConfiguration()
+        {
+            ToTable("Users");
+
+            HasKey(x => x.UserId);
+
+            Property(x => x.UserId)
+                .HasColumnName("UserID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            Property(x => x.Browser)
+                .HasColumnName("Browser")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(256)
+                .IsRequired();
+
+            Property(x => x.FName)
+                .HasColumnName("FName")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(128)
+                .IsOptional();
+
+            Property(x => x.LName)
+                .HasColumnName("LName")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(256)
+                .IsOptional();
+
+            Property(x => x.LoginPref)
+                .HasColumnName("LoginPref")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(256)
+                .IsRequired();
+
+            Property(x => x.VoteTotal)
+                .HasColumnName("VoteTotal")
+                .HasColumnType("bigint")
+                .IsRequired();
+
+            Property(x => x.About)
+                .HasColumnName("About")
+                .HasColumnType("text")
+                .IsOptional();
+
+            Property(x => x.Pseudonym)
+                .HasColumnName("Pseudonym")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(256)
+                .IsOptional();
+
+        }
+    }
+
+}
---
+++ Milestones/Milestone3/DiscussionHub/DiscussionHub/DAL/DiscussionHubContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using DiscussionHub.DAL.Configurations;
+using DiscussionHub.Models;
+
+namespace DiscussionHub.DAL
+{
+    public class DiscussionHubContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+        public DbSet<Discussions> Discussions { get; set; }
+
+        public DiscussionHubContext() : base("name=DiscussionHubContext") { }
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            // Model configurations to map entity to model
+            builder.Configurations.Add(new UsersConfiguration());
+            builder.Configurations.Add(new CommentsConfiguration());
+            builder.Configurations.Add(new DiscussionsConfiguration());
+        }
+
+    }
+}
