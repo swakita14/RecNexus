bdd016f5f97933fc66ffbb8c6bb0fbf1203a8884
Shion Wakita <swakita14@wou.edu>
2019-01-28 11:29:56 -0800

    added some interaction between the app and dB

--- Milestones/Milestone3/DiscussionHub/DiscussionHub/Models/Users.cs
+++ Milestones/Milestone3/DiscussionHub/DiscussionHub/Models/Users.cs
@@ -11,6 +11,8 @@
 
         public string LName { get; set; }
 
+        public string Email { get; set; }
+
         public string LoginPref { get; set; }
 
         public long VoteTotal { get; set; }
