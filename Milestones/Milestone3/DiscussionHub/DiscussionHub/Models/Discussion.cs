5ba9bfa2e8f1351486c4b1301d84246d7edc13c8
ABergman15 <abergman15@mail.wou.edu>
2019-02-02 20:16:16 -0800

    Finished Discussion Index

--- Milestones/Milestone3/DiscussionHub/DiscussionHub/Models/Discussion.cs
+++ Milestones/Milestone3/DiscussionHub/DiscussionHub/Models/Discussion.cs
@@ -5,24 +5,32 @@ namespace DiscussionHub.Models
 {
     public class Discussion
     {
-
+        
         public int DiscussionId { get; set; }
 
+        [Display(Name = "Total Votes")]
         public long VoteCount { get; set; }
 
+        [Display(Name = "Upvotes")]
         public long UpvoteCount { get; set; }
 
+        [Display(Name = "Downvotes")]
         public long DownvoteCount { get; set; }
 
+        [Display(Name = "Total Comments")]
         public long CommentCount { get; set; }
 
+        [Display(Name = "Total Views")]
         public long TotalViews { get; set; }
 
+       
         public int Rank { get; set; }
 
         public int UserId { get; set; }
 
+        [Display(Name = "Article")]
         public string ArticleLink { get; set; }
+
 
         public string Title { get; set; }
 
