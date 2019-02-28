7bd2ec403ee5c692a2426f8eecea9b24a0afbfab
Shion Wakita <swakita14@wou.edu>
2019-02-27 20:08:59 -0800

    added enum on viewmodel

---
+++ Capstone/PickUpSports/PickUpSports/Models/ViewModel/TimePreferenceViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PickUpSports.Models.ViewModel
+{
+    public enum DayOfWeek : byte
+    {
+        Sunday = 1,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thurday,
+        Friday,
+        Saturday
+    }
+
+    public class TimePreferenceViewModel
+    {
+        public int TimePrefID { get; set; }
+
+        public DayOfWeek DayOfWeek { get; set; }
+
+        public DateTime? BeginTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        public int ContactID { get; set; }
+    }
+}
