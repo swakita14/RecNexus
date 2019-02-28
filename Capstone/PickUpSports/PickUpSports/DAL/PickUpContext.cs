faf30bf68050f08d0d505a7e1d7fc9299eb3042e
Shayna Conner <sconner15@wou.edu>
2019-02-27 22:36:59 -0800

    Fix another conflict

--- Capstone/PickUpSports/PickUpSports/DAL/PickUpContext.cs
+++ Capstone/PickUpSports/PickUpSports/DAL/PickUpContext.cs
@@ -6,20 +6,23 @@ namespace PickUpSports.DAL
 {
     public class PickUpContext : DbContext
     {
-        public PickUpContext()
-            : base("name=PickUpContext")
-        {
-
+        public PickUpContext(): base("name=PickUpContext"){
         }
 
         public DbSet<Contact> Contacts { get; set; }
-        public DbSet<Contact> TimePreference { get; set; }
+        public DbSet<TimePreference> TimePreferences { get; set; }
+        public DbSet<BusinessHours> BusinessHours { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+
 
         protected override void OnModelCreating(DbModelBuilder builder)
         {
             // Model configurations to map entity to model
             builder.Configurations.Add(new ContactConfiguration());
             builder.Configurations.Add(new TimePreferenceConfiguration());
+            builder.Configurations.Add(new BusinessHoursConfiguration());
+            builder.Configurations.Add(new VenueConfiguration());
+
         }
     }
 }
