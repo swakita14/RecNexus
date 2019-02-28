326f49f42387a776383bb2a1680a1383f3ef92b7
Shion Wakita <swakita14@wou.edu>
2019-02-27 19:23:08 -0800

    added config & model file, updated context

---
+++ Capstone/PickUpSports/PickUpSports/DAL/Configurations/TimePreferenceConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using PickUpSports.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace PickUpSports.DAL.Configurations
+{
+    public class TimePreferenceConfiguration : EntityTypeConfiguration<TimePreference>
+    {
+        public TimePreferenceConfiguration()
+        {
+            ToTable("TimePreference");
+
+            // Change as needed for primary key
+            HasKey(x => x.TimePrefID);
+
+            // Configure any foreign keys
+            Property(x => x.TimePrefID)
+                .HasColumnName("TimePrefID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            Property(x => x.DayOfWeek)
+                .HasColumnName("DayOfWeek")
+                .HasColumnType("tinyint")
+                .IsOptional();
+
+            Property(x => x.BeginTime)
+                .HasColumnName("BeginTime")
+                .HasColumnType("datetime")
+                .IsOptional();
+
+            Property(x => x.EndTime)
+                .HasColumnName("EndTime")
+                .HasColumnType("datetime")
+                .IsOptional();
+
+            Property(x => x.ContactID)
+                .HasColumnName("ContactID")
+                .HasColumnType("int")
+                .IsRequired();
+
+        }
+    }
+}
---
+++ Capstone/PickUpSports/PickUpSports/Models/TimePreference.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PickUpSports.Models
+{
+    public class TimePreference
+    {
+
+        public int TimePrefID { get; set; }
+
+        public byte? DayOfWeek { get; set; }
+
+        public DateTime? BeginTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        public int ContactID { get; set; }
+
+    }
+
+}
