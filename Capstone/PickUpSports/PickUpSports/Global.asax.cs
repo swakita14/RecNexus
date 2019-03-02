e2abe082c181d6e127f8c33fb5b25b8652b78c81
Shayna Conner <sconner15@wou.edu>
2019-03-01 18:59:18 -0800

    More comments

--- Capstone/PickUpSports/PickUpSports/Global.asax.cs
+++ Capstone/PickUpSports/PickUpSports/Global.asax.cs
@@ -21,27 +21,30 @@ namespace PickUpSports
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+            // Register all controllers in our MVC project 
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
 
-            // Register RestClient for API
+            // Register RestClient for API with base URL 
             builder.Register(x =>
                     new RestClient($"https://maps.googleapis.com/maps/api/place"))
                 .Keyed<IRestClient>("Google");
 
+            // Get Google API key from app settings
             var placesKey = System.Web.Configuration.WebConfigurationManager.AppSettings["GooglePlacesApiKey"];
 
-            // Register API 
+            // Register API client class with the RestClient and API key as a parameter
             builder.Register(x => new PlacesApiClient(
                 x.ResolveKeyed<IRestClient>("Google"), placesKey
             )).As<IPlacesApiClient>();
 
-            // Register PickUpContext
+            // Register PickUpContext for database
             builder.Register(context => new PickUpContext())
                 .AsSelf()
                 .InstancePerLifetimeScope();
 
             var container = builder.Build();
 
+            // Resolve all registrations above 
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
