2f8dbaaf47d4275558853e2c5c13bfd27d357272
Shayna Conner <sconner15@wou.edu>
2019-03-12 21:22:45 -0700

    Pass geolocation to controller

---
+++ Capstone/PickUpSports/PickUpSports/Models/ViewModel/SortByViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PickUpSports.Models.ViewModel
+{
+    public class SortByViewModel
+    {
+        public string SortType { get; set; }
+
+        public string CurrentLatitude { get; set; }
+
+        public string CurrentLongitude { get; set; }
+    }
+}
