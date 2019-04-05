ea96aa916f8210c5cb07465646373dac7c5f169c
Shayna Conner <sconner15@wou.edu>
2019-04-04 20:03:51 -0700

    Start test project and write tests

--- Capstone/PickUpSports/PickUpSports/Controllers/GameController.cs
+++ Capstone/PickUpSports/PickUpSports/Controllers/GameController.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Web.Mvc;
 using PickUpSports.DAL;
+using PickUpSports.Models.DatabaseModels;
 using PickUpSports.Models.ViewModel.GameController;
 
 namespace PickUpSports.Controllers
@@ -26,8 +30,45 @@ namespace PickUpSports.Controllers
         [HttpPost]
         public ActionResult CreateGame(CreateGameViewModel model)
         {
-            // Still need logic here to create the game
+            if (ModelState.IsValid) return View(model);
+
+            // Get venue by ID and business hours for that venue
+            Venue venue = _context.Venues.Find(model.VenueId);
+            List<BusinessHours> venueHours = _context.BusinessHours.Where(b => b.VenueId == venue.VenueId).ToList();
+
+            // Get start and end dates
+            var dates = model.DateRange.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+            var startDateTime = DateTime.Parse(dates[0], CultureInfo.CurrentCulture);
+            var endDateTime = DateTime.Parse(dates[1], CultureInfo.CurrentCulture);
+
+            
+
             return View();
+        }
+
+        public bool VenueIsAvailable(List<BusinessHours> venueHours, DateTime startDateTime, DateTime endDateTime)
+        {
+            // If no business hours then venue has no hours and is therefore not available
+            if (venueHours == null) return false;
+
+            // Only checking start date because games should not span over a day 
+            DayOfWeek startDate = startDateTime.DayOfWeek;
+            BusinessHours venueOpenDate = venueHours.FirstOrDefault(x => x.DayOfWeek == (int) startDate);
+
+            // Venue is open that date, check timeframes
+            if (venueOpenDate != null)
+            {
+                TimeSpan startTime = startDateTime.TimeOfDay;
+                TimeSpan endTime = endDateTime.TimeOfDay;
+
+                // Ensure both start and end times are within range
+                if (startTime > venueOpenDate.OpenTime && startTime < venueOpenDate.CloseTime)
+                {
+                    if (endTime > venueOpenDate.OpenTime && endTime < venueOpenDate.CloseTime) return true;
+                }
+            }
+
+            return false;
         }
     }
 }
---
+++ Capstone/PickUpSports/PickUpSportsTests/GameControllerTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using PickUpSports.Controllers;
+using PickUpSports.DAL;
+using PickUpSports.Models.DatabaseModels;
+
+namespace PickUpSportsTests
+{
+    [TestFixture]
+    public class GameControllerTests
+    {
+        private readonly GameController _sut;
+        
+        public GameControllerTests()
+        {
+            // Mock Context to pass into service under test (GameController)
+            var contextMock = new Mock<PickUpContext>();
+            _sut = new GameController(contextMock.Object);
+        }
+
+        [Test]
+        public void VenueIsAvailable_GivenValidDateRange_ReturnsTrue()
+        {
+            // Arrange
+            List<BusinessHours> venueHours = new List<BusinessHours>();
+            venueHours.Add(new BusinessHours{DayOfWeek = 1, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00")});
+            venueHours.Add(new BusinessHours { DayOfWeek = 2, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00") });
+            venueHours.Add(new BusinessHours { DayOfWeek = 3, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00") });
+
+            DateTime startDateTime = DateTime.Parse("2019-04-01 01:00 PM");
+            DateTime endDateTime = DateTime.Parse("2019-04-01 04:00 PM");
+
+            // Act
+            bool isVenueAvailable = _sut.VenueIsAvailable(venueHours, startDateTime, endDateTime);
+
+            // Assert
+            Assert.AreEqual(isVenueAvailable, true);
+        }
+
+        [Test]
+        public void VenueIsAvailable_GivenUnavailableDayOfWeek_ReturnsFalse()
+        {
+            // Arrange
+            List<BusinessHours> venueHours = new List<BusinessHours>();
+            venueHours.Add(new BusinessHours
+                {DayOfWeek = 2, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00")});
+            venueHours.Add(new BusinessHours
+                {DayOfWeek = 3, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00")});
+
+            DateTime startDateTime = DateTime.Parse("2019-04-01 01:00 PM");
+            DateTime endDateTime = DateTime.Parse("2019-04-01 04:00 PM");
+
+            // Act
+            bool isVenueAvailable = _sut.VenueIsAvailable(venueHours, startDateTime, endDateTime);
+
+            // Assert
+            Assert.AreEqual(isVenueAvailable, false);
+
+        }
+
+        [Test]
+        public void VenueIsAvailable_GivenNonBusinessStartTime_ReturnsFalse()
+        {
+            // Arrange
+            List<BusinessHours> venueHours = new List<BusinessHours>();
+            venueHours.Add(new BusinessHours { DayOfWeek = 1, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00") });
+            venueHours.Add(new BusinessHours { DayOfWeek = 2, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00") });
+            venueHours.Add(new BusinessHours { DayOfWeek = 3, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00") });
+
+            DateTime startDateTime = DateTime.Parse("2019-04-01 04:00 AM");
+            DateTime endDateTime = DateTime.Parse("2019-04-01 04:00 PM");
+
+            // Act
+            bool isVenueAvailable = _sut.VenueIsAvailable(venueHours, startDateTime, endDateTime);
+
+            // Assert
+            Assert.AreEqual(isVenueAvailable, false);
+        }
+
+        [Test]
+        public void VenueIsAvailable_GivenNonBusinessEndTime_ReturnsFalse()
+        {
+            // Arrange
+            List<BusinessHours> venueHours = new List<BusinessHours>();
+            venueHours.Add(new BusinessHours { DayOfWeek = 1, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00") });
+            venueHours.Add(new BusinessHours { DayOfWeek = 2, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00") });
+            venueHours.Add(new BusinessHours { DayOfWeek = 3, OpenTime = TimeSpan.Parse("05:00"), CloseTime = TimeSpan.Parse("22:00") });
+
+            DateTime startDateTime = DateTime.Parse("2019-04-01 1:00 PM");
+            DateTime endDateTime = DateTime.Parse("2019-04-01 11:00 PM");
+
+            // Act
+            bool isVenueAvailable = _sut.VenueIsAvailable(venueHours, startDateTime, endDateTime);
+
+            // Assert
+            Assert.AreEqual(isVenueAvailable, false);
+        }
+
+        [Test]
+        public void VenueIsAvailable_BusinessDoesNotHaveHours_ReturnsFalse()
+        {
+            // Arrange
+            List<BusinessHours> venueHours = new List<BusinessHours>();
+
+            DateTime startDateTime = DateTime.Parse("2019-04-01 04:00 AM");
+            DateTime endDateTime = DateTime.Parse("2019-04-01 04:00 PM");
+
+            // Act
+            bool isVenueAvailable = _sut.VenueIsAvailable(venueHours, startDateTime, endDateTime);
+
+            // Assert
+            Assert.AreEqual(isVenueAvailable, false);
+        }
+    }
+}
