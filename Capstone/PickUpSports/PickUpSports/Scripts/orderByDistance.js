7855b634ce98d435e539325f5dce458b8c97688e
ABergman15 <abergman15@mail.wou.edu>
2019-04-14 18:11:38 -0700

    Incoming broken js

--- Capstone/PickUpSports/PickUpSports/Scripts/orderByDistance.js
+++ Capstone/PickUpSports/PickUpSports/Scripts/orderByDistance.js
@@ -1,29 +1,17 @@
-﻿function getLocation() {
+﻿var update = function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
+
+   
 }
 
 function showPosition(position) {
-    var lat = position.coords.latitude;
-    var long = position.coords.longitude;
-    jQuery.ajax({
-        type: "POST",
-        url: "../../Venue/Index/",
-        dataType: "json",
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify({ curLat: lat, curLong: long, sortBy: "Distance" }),
-        success: function (data) {
-            alert(data);
-        },
-        failure: function (errMsg) {
-            alert(errMsg);
-        }
-    });
+    document.getElementById('CurrentLatitude').value = position.coords.latitude;
+    document.getElementById('CurrentLongitude').value = position.coords.longitude;
+    }
 
+window.onload = update;
 
-}
-
-getLocation();
