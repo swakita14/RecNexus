6ceb3e2584c73838c978510edc3b64937c7bad70
Shayna Conner <sconner15@wou.edu>
2019-05-03 00:12:12 -0700

    Fix test

--- Capstone/PickUpSports/PickUpSportsTests/GameControllerTests.cs
+++ Capstone/PickUpSports/PickUpSportsTests/GameControllerTests.cs
@@ -12,20 +12,20 @@ namespace PickUpSportsTests
     [TestFixture]
     public class GameControllerTests
     {
-        private readonly Mock<PickUpContext> _contextMock;
         private readonly Mock<IContactService> _contactServiceMock;
         private readonly Mock<IGMailService> _gmailServiceMock;
         private readonly Mock<IGameService> _gameServiceMock;
+        private readonly Mock<IVenueService> _venueServiceMock;
 
         private readonly GameController _sut;
 
         public GameControllerTests()
         {
-            _contextMock = new Mock<PickUpContext>();
             _contactServiceMock = new Mock<IContactService>();
             _gmailServiceMock = new Mock<IGMailService>();
             _gameServiceMock = new Mock<IGameService>();
-            _sut = new GameController(_contextMock.Object, _contactServiceMock.Object, _gmailServiceMock.Object, _gameServiceMock.Object);
+            _venueServiceMock = new Mock<IVenueService>();
+            _sut = new GameController(_contactServiceMock.Object, _gmailServiceMock.Object, _gameServiceMock.Object, _venueServiceMock.Object);
         }
 
         /**
