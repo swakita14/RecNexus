027c98ab8c88a1291c1fac3a4fe4ebb13fc12c3f
ABergman15 <abergman15@mail.wou.edu>
2019-05-10 16:36:06 -0700

    Added services and model for Trending page

---
+++ Capstone/PickUpSports/PickUpSports/Models/ViewModel/TrendingController/TrendingModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PickUpSports.Models.ViewModel
+{
+    public class TrendingViewModel
+    {
+    }
+}
